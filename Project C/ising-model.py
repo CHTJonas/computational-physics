c660a649b63a1754724342d6a0d2e41dd6429021
Charlie Jonas <devel@charliejonas.co.uk>
2018-04-18 14:12:36 +0100

    Update imports for consistency with literature

--- Project C/ising-model.py
+++ Project C/ising-model.py
@@ -3,8 +3,8 @@
 import matplotlib
 matplotlib.use('Agg')
 import math
-import numpy
-import matplotlib.pyplot
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
 
 # Total number of Monte Carlo steps
@@ -12,8 +12,8 @@ steps = 100
 # The dimensions of the NxN lattice
 N = 15
 # Array of spin-up atoms
-spins = numpy.empty(shape=(N,N))
-for (x,y), value in numpy.ndenumerate(spins):
+spins = np.empty(shape=(N,N))
+for (x,y), value in np.ndenumerate(spins):
   spins[x,y] = 1.0
 # The magnetic moment
 mu = 1.0;
@@ -26,7 +26,7 @@ kB = 1.0
 # So we measure temperature in units of J/kB
 
 def sweep( temp ):
-  for (x,y), value in numpy.ndenumerate(spins):
+  for (x,y), value in np.ndenumerate(spins):
     # Flip the spin and calculate the energy difference
     E1 = energy()
     spins[x,y] = -spins[x,y]
@@ -37,7 +37,7 @@ def sweep( temp ):
       # Spin is already flipped
       spins[x,y] = spins[x,y]
     else:
-      p = numpy.random.random()
+      p = np.random.random()
       if prob(Edelta, temp) > p:
         # Spin is already flipped
         spins[x,y] = spins[x,y]
@@ -48,7 +48,7 @@ def sweep( temp ):
 
 def energy():
   E = 0
-  for (x,y), value in numpy.ndenumerate(spins):
+  for (x,y), value in np.ndenumerate(spins):
     # Periodic boundary conditions
     # Imagine the lattice on a torus
     E -= J * ( spins[x-1,y]*spins[x,y] + spins[x,y-1]*spins[x,y] + spins[x,(y+1)%N]*spins[x,y] + spins[(x+1)%N,y]*spins[x,y] )
@@ -57,17 +57,17 @@ def energy():
 
 def magnetisation():
   M = 0.0
-  for (x,y), value in numpy.ndenumerate(spins):
+  for (x,y), value in np.ndenumerate(spins):
     M += float(spins[x,y])
   M = float(M) / float(N)
   return M;
 
 def prob( deltaE, temp ):
-  p = numpy.exp( -float(deltaE) / float(kB) / float(temp) )
+  p = np.exp( -float(deltaE) / float(kB) / float(temp) )
   return p;
 
-energies = numpy.empty(shape=(steps))
-magnetisations = numpy.empty(shape=(steps))
+energies = np.empty(shape=(steps))
+magnetisations = np.empty(shape=(steps))
 
 temp = sys.argv[1]
 for n in range(steps):
